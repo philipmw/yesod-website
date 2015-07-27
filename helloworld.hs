c5d594d4ce5eb85e2fc388896c481dcfcca12b0c
Philip White <pmw@amazon.com>
2015-07-27 12:22:26 -0700

    Support default port

--- helloworld.hs
+++ helloworld.hs
@@ -6,11 +6,14 @@
 import Prelude hiding (length)
 import Yesod
 import Yesod.Form
+import Control.Exception (tryJust)
+import Control.Monad (guard)
 import Data.Text (Text, length, pack)
 import Data.Text.Read (decimal)
 import System.Environment
 import System.IO (hPutStrLn, stderr)
 import System.Exit
+import System.IO.Error (isDoesNotExistError)
 
 data HelloWorld = HelloWorld
 
@@ -49,26 +52,31 @@ postGreetR = do
         FormFailure _ -> defaultLayout [whamlet|Hmm, form failure.|]
         _ -> defaultLayout [whamlet|Hmm, something was wrong.|]
 
-readPort :: IO (Maybe Int)
+defaultPort = 80
+
+readPort :: IO (Int)
 readPort = do
-    portStr <- fmap pack $ getEnv "PORT"
-    let eitherPort = decimal portStr
-    case eitherPort of
+    r <- tryJust (guard . isDoesNotExistError) $ getEnv "PORT"
+    case r of
         Left error -> do
-            hPutStrLn stderr ("Could not get PORT: " ++ error)
-            return Nothing
-        Right (port, remainder) -> do
-            if (length remainder) > 0 then do
-                hPutStrLn stderr "Port is not an integer"
-                return Nothing
-            else
-                return $ Just port
+            hPutStrLn stderr ("Could not get PORT: " ++ show error)
+            return defaultPort
+        Right portStr -> do
+            let portText = pack portStr
+            let eitherPort = decimal portText
+            case eitherPort of
+                Left error -> do
+                    hPutStrLn stderr ("Port is not an integer: " ++ error)
+                    return defaultPort
+                Right (port, remainder) -> do
+                    if (length remainder) > 0 then do
+                        hPutStrLn stderr "Port is not an integer; what's that leftover crap?"
+                        return defaultPort
+                    else
+                        return port
 
 main :: IO()
 main = do
-        maybePort <- readPort
-        case maybePort of
-            Nothing -> exitWith $ ExitFailure 1
-            Just port -> do
-                putStrLn $ "Listening on port " ++ show port
-                warp port HelloWorld
+        port <- readPort
+        putStrLn $ "Listening on port " ++ show port
+        warp port HelloWorld
