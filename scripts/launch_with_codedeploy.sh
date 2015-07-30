ec93c4df38cbcb8aaae92eabfabfd8c91a7c72f4
Philip White <pmw@amazon.com>
2015-07-29 17:36:58 -0700

    Pipe log to syslog; start webserver in background

--- scripts/launch_with_codedeploy.sh
+++ scripts/launch_with_codedeploy.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-/opt/yesod-website/yesod-website
+
+cd /opt/yesod-website
+mkfifo syslog.out
+mkfifo syslog.err
+
+nohup logger -p daemon.info -t yesod-hello -f syslog.out &
+nohup logger -p daemon.err -t yesod-hello -f syslog.err &
+nohup /opt/yesod-website/yesod-website 2>syslog.err >syslog.out &
