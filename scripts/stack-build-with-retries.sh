fc631d803dd518cdc264eb29651f876c715c7427
Philip White <pmw@amazon.com>
2015-08-01 10:07:24 -0700

    Increase retries

--- scripts/stack-build-with-retries.sh
+++ scripts/stack-build-with-retries.sh
@@ -3,10 +3,11 @@
 TRY=0
 RETCODE=1
 
-while [ $TRY -lt 3 ] && [ $RETCODE -ne 0 ]; do
+while [ $TRY -lt 10 ] && [ $RETCODE -ne 0 ]; do
   TRY=$(($TRY + 1))
   stack build
   RETCODE=$?
 done
 
 echo "Done on try $TRY with return code $RETCODE"
+exit $RETCODE
