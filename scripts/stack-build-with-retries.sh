7ae0cc74c4928b243dd09cfed883ed48008e76fe
Philip White <pmw@amazon.com>
2015-07-31 23:55:44 -0700

    and add the right script

---
+++ scripts/stack-build-with-retries.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TRY=0
+RETCODE=1
+
+while [ $TRY -lt 3 ] && [ $RETCODE -ne 0 ]; do
+  TRY=$(($TRY + 1))
+  stack build
+  RETCODE=$?
+done
+
+echo "Done on try $TRY with return code $RETCODE"
